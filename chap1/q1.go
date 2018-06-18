82145b1c331fc6c6f0631cc38bbb1dc7c5bec326
paul wisehart <paul@oldcode.org>
2018-06-18 14:23:19 -0400

    asd

---
+++ chap1/q1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var q = `
+Implement an algo.  to determine if a string has all unique characters.
+What if you cannot use addl. data structures?
+`
+
+func main() {
+	fmt.Printf("%s...\n", os.Args[0])
+}
