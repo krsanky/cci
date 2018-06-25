27d418a6b4b0c739f08a3d63c95c94f008b666bd
paul wisehart <paul@oldcode.org>
2018-06-25 17:23:35 -0400

    ...

--- _chap1_q3.cpp
+++ _chap1_q3.cpp
@@ -1,43 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 
-void s1();
 void URLify(char *);
+int find_first_space(char *);
+int find_first_space_buffer(char *);
 
 int main(int argc, char * argv[]) {
 	//printf("%s: num-args:%d\n", argv[0], argc);
-	// 	ss := []string{"asd 1 23    ", "q  weq  we        ", "d fsW  "}
-	s1();
-	char s2[] = "test string 1  ";
+
+	char s1[] = "str str str    ";
+	char s2[] = "test string 1    ";
+	char s3[] = "asd 1 23    ";
+	char s4[] = "q  weq  we        ";
+	char s5[] = "d fsW  ";
+	char s6[] = "A  ";
+	//if (strlen(s) > 1) s[1] = 'X';
+
+	//URLify(s1);
 	URLify(s2);
+	//URLify(s3);
+
 	return 0;
 }
 
 void URLify(char * s) {
-	s[1] = 'x';
-	printf("URLify:[%s] length:%zu\n", s, strlen(s));
-	for (int i=0; i<strlen(s); i++) {
-		printf("%2d:%c\n", i, s[i]);
+	int first = 0;
+	size_t last = strlen(s);
+	printf("URLify:[%s] length:%zu\n", s, last);
+	//for (int i=0; i<last; i++) printf("%2d: %c\n", i, s[i]);
 
-	}
+	printf("1st space buf:%d\n", find_first_space_buffer(s));
+	
+	printf("space at: %d\n", find_first_space(s));
+	printf("shift string over 2...\n");
+	
 }
 
+int find_first_space_buffer(char * s) {
+	for (int i=strlen(s)-1; i>=0; i--) {
+		printf("%2d: %c\n", i, s[i]);
+		if (s[i] != ' ') {
+			//printf("no space?\n");
+			return i;
+		}
+	}
+	return -1;
+}
 
-void s1() {
-	char ch_arr[3][10] = {
-    	"spike",
-		"tom",
-		"jerry"
-	};
-/*
-	char *sports[] = {
-		"golf",
-		"hockey",
-		"football",
-		"cricket",
-		"shooting"
-	};
-*/
+int find_first_space(char * s) {
+	for (int i=0; i<strlen(s); i++) 
+		if (s[i] == ' ') 
+			return i;
+	return -1;
 }
 
 
