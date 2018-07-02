4c875ab4b147fe2caeb162703aa27b37e4a16fdb
paul wisehart <paul@oldcode.org>
2018-07-02 11:44:35 -0400

    change question to include padding the needed spaces onto the end of the word

--- _chap1_q3.cpp
+++ _chap1_q3.cpp
@@ -2,20 +2,24 @@
 #include <string.h>
 
 void URLify(char *);
-int find_first_space(char *);
-int find_first_space_buffer(char *);
+int count_spaces(char *);
 
 int main(int argc, char * argv[]) {
 	//printf("%s: num-args:%d\n", argv[0], argc);
-
+/*
 	char s1[] = "str str str    ";
 	char s2[] = "test string 1    ";
 	char s3[] = "asd 1 23    ";
 	char s4[] = "q  weq  we        ";
 	char s5[] = "d fsW  ";
 	char s6[] = "A  ";
-	//if (strlen(s) > 1) s[1] = 'X';
-
+*/
+	char s1[] = "str str str";
+	char s2[] = "test string 1";
+	char s3[] = "asd 1 23";
+	char s4[] = "q  weq  we";
+	char s5[] = "d fsW ";
+	char s6[] = "A";
 	//URLify(s1);
 	URLify(s2);
 	//URLify(s3);
@@ -29,29 +33,17 @@ void URLify(char * s) {
 	printf("URLify:[%s] length:%zu\n", s, last);
 	//for (int i=0; i<last; i++) printf("%2d: %c\n", i, s[i]);
 
-	printf("1st space buf:%d\n", find_first_space_buffer(s));
-	
-	printf("space at: %d\n", find_first_space(s));
-	printf("shift string over 2...\n");
+	printf("num spaces:%d\n", count_spaces(s));
 	
 }
 
-int find_first_space_buffer(char * s) {
-	for (int i=strlen(s)-1; i>=0; i--) {
-		printf("%2d: %c\n", i, s[i]);
-		if (s[i] != ' ') {
-			//printf("no space?\n");
-			return i;
-		}
-	}
-	return -1;
+int count_spaces(char * s) {
+    int n = 0;
+	for (int i=0; i<strlen(s); i++) {
+		if (s[i] == ' ') {
+            n++;
+        }
+    }
+    return n;
 }
-
-int find_first_space(char * s) {
-	for (int i=0; i<strlen(s); i++) 
-		if (s[i] == ' ') 
-			return i;
-	return -1;
-}
-
 
