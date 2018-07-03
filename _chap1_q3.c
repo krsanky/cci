c28532f8be4d0670af4c5f55988464e2d650a8b8
paul wisehart <paul@oldcode.org>
2018-07-03 19:28:00 -0400

    is this openbsd style?, (return char * ?)

--- _chap1_q3.c
+++ _chap1_q3.c
@@ -11,14 +11,6 @@ int
 main(int argc, char * argv[]) 
 {
 	//printf("%s: num-args:%d\n", argv[0], argc);
-/*
-	char s1[] = "str str str    ";
-	char s2[] = "test string 1    ";
-	char s3[] = "asd 1 23    ";
-	char s4[] = "q  weq  we        ";
-	char s5[] = "d fsW  ";
-	char s6[] = "A  ";
-*/
 	char s1[] = "str str str";
 	char s2[] = "test string 1";
 	char s3[] = "asd 1 23";
@@ -34,7 +26,9 @@ main(int argc, char * argv[])
 	return 0;
 }
 
-void URLify(char * s) {
+void 
+URLify(char * s) 
+{
 	int first = 0;
 	size_t last = strlen(s);
 	printf("URLify:[%s] length:%zu\n", s, last);
@@ -46,7 +40,9 @@ void URLify(char * s) {
 	
 }
 
-int count_spaces(char * s) {
+int 
+count_spaces(char * s) 
+{
     int n = 0;
 	for (int i=0; i<strlen(s); i++) {
 		if (s[i] == ' ') {
@@ -56,7 +52,9 @@ int count_spaces(char * s) {
     return n;
 }
 
-void allocate_test() {
+void 
+allocate_test() 
+{
 	int *array = malloc(10 * sizeof(int));
 	if (array == NULL) {
 		fprintf(stderr, "malloc failed\n");
@@ -67,7 +65,9 @@ void allocate_test() {
 
 // https://man.openbsd.org/strlcpy.3
 // https://www.sudo.ws/todd/papers/strlcpy.html
-char * copy_add_trailing_spaces(char * s, int spaces) {
+char *
+copy_add_trailing_spaces(char * s, int spaces) 
+{
 	char spc = 'X';
 	int nspaces = count_spaces(s);
     int len = ((strlen(s) + nspaces) * sizeof(char)) + 1; // +1 for null terminator?
