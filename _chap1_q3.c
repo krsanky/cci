06daf29014b2302f444ae6e6c26ff4c0a33dc595
paul wisehart <paul@oldcode.org>
2018-07-04 18:06:39 -0400

    asd

--- _chap1_q3.c
+++ _chap1_q3.c
@@ -2,9 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-void     URLify(char *);
-int      count_spaces(char *);
-char    *copy_add_trailing_spaces(char *, int);
+char    *URLify(const char *);
+int      count_spaces(const char *);
+char    *copy_add_trailing_spaces(const char *, int);
+void	 teststuff();
+void	 push_right(char *, int, int, int); 
 
 /* 
  * All major routines should have a comment briefly describing what 
@@ -30,38 +32,52 @@ main(int argc, char * argv[])
 	    "a top"
 	};
     nwords = sizeof(words)/sizeof(words[0]);
-	
-	//URLify(s2);
-	//URLify(s7);
-    printf("1st word:%zu array of words:%zu\n", sizeof(words[0]), nwords);
+
+    char * s1 = copy_add_trailing_spaces(words[0], count_spaces(words[0]));
+ 	push_right(s1, 3, 2, strlen(words[0])); // push right 2 spaces at index 3
+	printf("[%s]PUSH_RIGHT[%s]", words[0], s1);
 
     for (int i=0; i<nwords; i++) {
-	    URLify(words[i]);
-        /* printf("word[%s]\n", words[i]); */
+	    printf("word[%s] url[%s]\n", words[i], URLify(words[i]));
     }    
 
 	return 0;
 }
 
-void 
-URLify(char * s) 
+void	
+teststuff() 
 {
-	int first;
-	size_t last;
+	printf("\n");
+	printf("sizeof('\\0'):%lu\n", sizeof('\0'));
+	printf("sizeof(char):%lu\n", sizeof(char));
+	printf("sizeof('a'):%lu\n", sizeof('a'));
+	printf("\n");
+}
 
-	first = 0;
-	last = strlen(s);
+char *
+URLify(const char * s) 
+{
+	char * u;
+	int orig_len;
 
-	printf("URLify:[%s] length:%zu\n", s, last);
-	//for (int i=0; i<last; i++) printf("%2d: %c\n", i, s[i]);
+    u = copy_add_trailing_spaces(s, count_spaces(s));
 
-	printf("num spaces:%d\n", count_spaces(s));
-    char * cpy = copy_add_trailing_spaces(s, 4);
-    free(cpy);
+	for (int i=0; i<strlen(s); i++) {
+		/*
+		printf("[%c]", u[i]); 
+		if (u[i] == ' ') {
+			printf("SPACE");
+		}
+		*/
+		
+	}	
+	printf("\n");
+
+	return u;
 }
 
 int 
-count_spaces(char * s) 
+count_spaces(const char * s) 
 {
     int n = 0;
 	for (int i=0; i<strlen(s); i++) {
@@ -77,21 +93,31 @@ count_spaces(char * s)
  * https://www.sudo.ws/todd/papers/strlcpy.html
  */
 char *
-copy_add_trailing_spaces(char * s, int spaces) 
+copy_add_trailing_spaces(const char * s, int spaces) 
 {
 	int nspaces, len;
     char * copy;
 
 	nspaces = count_spaces(s);
-    len = ((strlen(s) + nspaces) * sizeof(char)) + 1; // +1 for null terminator?
+    /* len = (strlen(s) + (2*nspaces) + 1) * sizeof(char); */
+    len = strlen(s) + (2*nspaces) + 1;
     copy = malloc(len);
 
     strlcpy(copy, s, len);
     for (int i=0; i<nspaces; i++) {
-        strlcat(copy, " ", len);
+        strlcat(copy, "  ", len);
     }
-    printf("copy[%s] tried for length:%d copy length:%zu\n", copy, len, strlen(copy));
+    /* printf("copy[%s] tried for length:%d copy length:%zu\n", copy, len, strlen(copy)); */
     return copy;
 }
 
+/*
+ * Move all characters in the string to the right by 'count' spaces.
+ * Do this at location 'loc'.
+ */
+void
+push_right(char * s, int loc, int count, int orig_len) {
+	printf("loc:%d count:%d orig_len:%d\n", loc, count, orig_len);
+	  
+}
 
