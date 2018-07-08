ce8c64285aa9cd81ece4e765ececcf7af4fe6602
paul wisehart <paul@oldcode.org>
2018-07-07 22:51:39 -0400

    ...

--- _chap1_q3.c
+++ _chap1_q3.c
@@ -6,10 +6,7 @@
 char    *URLify(const char *);
 int      count_spaces(const char *);
 char    *copy_add_trailing_spaces(const char *, int);
-void	 teststuff();
 void	 push_right(char *, int, int, int); 
-
-bool debug = false;
 
 /* 
  * All major routines should have a comment briefly describing what 
@@ -36,12 +33,16 @@ main(int argc, char * argv[])
 	};
     nwords = sizeof(words)/sizeof(words[0]);
 
+    char * w2 = "WORD TWO";
+    char * s2 = copy_add_trailing_spaces(w2, count_spaces(w2)*2);
+    printf("s2[%s]\n", s2);
+    push_right(s2, 4, 10, 2);
 
-    char * s1 = copy_add_trailing_spaces(words[1], count_spaces(words[1]));
-    push_right(s1, 4, 10, 2);
-
-    for (int i=0; i<nwords; i++) 
-        printf("word[%s] url[%s]\n", words[i], URLify(words[i]));
+    /*
+    for (int i=0; i<nwords; i++) {
+        copy_add_trailing_spaces(words[i], count_spaces(words[i])*2);
+        //printf("word[%s] url[%s]\n", words[i], URLify(words[i]));
+    }*/
 
 	return 0;
 }
@@ -54,8 +55,9 @@ URLify(const char * s)
 
     orig_len = end_cap = strlen(s);
     u = copy_add_trailing_spaces(s, count_spaces(s));
+    printf("u[%s]\n", u);
 
-	for (int i=0; i<strlen(s); i++) {
+	for (int i=0; i<strlen(u)-1; i++) {
 		printf("[%c]", u[i]); 
 		if (u[i] == ' ') {
 			printf("SPACE");
@@ -92,51 +94,33 @@ count_spaces(const char * s)
 }
 
 /*
- * https://man.openbsd.org/strlcpy.3
- * https://www.sudo.ws/todd/papers/strlcpy.html
+ * Return copy of 's' with 'spaces' number of spaces
+ * added to the end.
  */
 char *
 copy_add_trailing_spaces(const char * s, int spaces) 
 {
-	int nspaces, len;
+	int len;
     char * copy;
 
-	nspaces = count_spaces(s);
-    /* len = (strlen(s) + (2*nspaces) + 1) * sizeof(char); */
-    len = strlen(s) + (2*nspaces) + 1;
+    len = strlen(s) + spaces + 1;
     copy = malloc(len);
 
     strlcpy(copy, s, len);
-    for (int i=0; i<nspaces; i++) {
-        strlcat(copy, "  ", len);
+    for (int i=0; i<spaces; i++) {
+        strlcat(copy, " ", len);
     }
-    /* printf("copy[%s] tried for length:%d copy length:%zu\n", copy, len, strlen(copy)); */
+    printf("copy[%s] tried for length:%d copy length:%zu\n", copy, len, strlen(copy)); 
     return copy;
 }
 
-/*
+/* DO this for the general case.
  * Move the sub-string [start, end] to the right by 'count' spaces.
- *
-[test string 1    ]
-[XXXX stringXXXXXX]
  */
 void
 push_right(char * s, int start, int end, int count) {
-    if (debug) {
-	    printf("[%s]\n", s);
-	    printf("[");
-        for (int i=0; i<strlen(s); i++) {
-            if ((i<start) || (i>end)) 
-                printf("%c", 'X');
-            else
-                printf("%c", s[i]);
-        }
-	    printf("]\n");
-    }
-	  
-    for (int i=end; i>start; i--) { 
+    for (int i=end; i>start; i--)  
         s[i+count] = s[i];
-    }
-    if (debug) printf("[%s]\n", s);
+    
 }
 
