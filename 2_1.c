b8fa6c68f65dc6436203cd940423a13449a38778
paul wisehart <paul@oldcode.org>
2019-02-15 14:25:05 -0500

    initial rm_head_dupes() code

--- 2_1.c
+++ 2_1.c
@@ -6,7 +6,7 @@
 #include "ll.h"
 
 ll_node *
-build_list()
+build_list(int len)
 {
 	ll_node		*n;
 	ll_node		*l;
@@ -21,7 +21,7 @@ build_list()
 	if (l == NULL)
 		return NULL;
 
-	for (int i=0; i<40; i++) {
+	for (int i=0; i<len; i++) {
 		r = random() % 100;
 		snprintf(buf, 20, "%ld", r);
 /*		printf("rand:%ld as-str:%s\n", r, buf);*/
@@ -34,16 +34,30 @@ build_list()
 	return l;
 }
 
+void
+rm_head_dupes(ll_node *l)
+{
+	ll_node		*h;
+	h = l;
+	while (l->next != NULL) {
+/*		printf("strcmp: %s %s\n", h->val, l->next->val);*/
+		if ((strcmp(h->val, l->next->val) == 0)) {
+			printf("MATCH: %s %s\n", h->val, l->next->val);
+		}
+		l = l->next;
+	}
+}
+
 int 
 main() 
 {
 	ll_node		*l;
 
 	printf("remove dupes\n");
-	if ((l = build_list()) == NULL)
+	if ((l = build_list(200)) == NULL)
 		return EXIT_FAILURE;
-
-	ll_print(l);
+	rm_head_dupes(l);
+/*	ll_print(l);*/
 	ll_free(l);
 	return EXIT_SUCCESS;
 }
