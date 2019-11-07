7362f3054aae07c8ab4447fe1c10886d385f14be
paul wisehart <paul@oldcode.org>
2019-11-07 18:43:51 -0500

    asd

--- C/16_03.c
+++ C/16_03.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Given 2 segments ([start-point, end-point]),
+ * where do they intersect if at all?
+ */
+
 int
 main(void)
 {
 	printf("16_03\n");
 	return EXIT_SUCCESS;
 }
-
