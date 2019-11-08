3feec3b311602e3fc44ef18fcee67820b4d741b0
paul wisehart <paul@oldcode.org>
2019-11-07 19:04:03 -0500

    asd

--- C/16_03.c
+++ C/16_03.c
@@ -9,6 +9,21 @@
 int
 main(void)
 {
-	printf("16_03\n");
+	double	s1p1, s1p2;
+	double	s2p1, s2p2;
+
+	printf("16_03...\n");
+
+	/*
+	scanf("%d", &nconv);
+	scanf("%lf %c", &val, &unit);
+	*/
+	printf("input segment one ( eg: 23.2 -0.5 ):\n");
+	scanf("%lf %lf", &s1p1, &s1p2);
+	printf("segment one [ %lf, %lf ]\n", s1p1, s1p2);
+	printf("input segment two ( eg: 23.2 -0.5 ):\n");
+	scanf("%lf %lf", &s2p1, &s2p2);
+	printf("segment two [ %lf, %lf ]\n", s2p1, s2p2);
+
 	return EXIT_SUCCESS;
 }
