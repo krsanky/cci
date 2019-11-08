80d430cc333fe26e93324ccef3bfa16907156559
paul wisehart <paul@oldcode.org>
2019-11-08 10:32:31 -0500

    go/c10/main.go

---
+++ go/c10/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+var c10q01 = `
+10.01 Sorted merge 
+Given 2 sorted groups A and B, 
+where A has enuf space for all of B.
+Merge B into A sorted.`
+
+func Q01() {
+	fmt.Printf("%s:\n", "question 10.01")
+}
+
+func main() {
+	Q01()
+}
