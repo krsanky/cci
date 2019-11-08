5b542af1bbb6c859b7c811c40bbe53d17b1c2a23
paul wisehart <paul@oldcode.org>
2019-11-08 10:31:05 -0500

    asd

--- C/16_03.c
+++ C/16_03.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,24 +7,51 @@
  * where do they intersect if at all?
  */
 
+/*
+https://www.youtube.com/watch?v=4bIsntTiKfM
+slope = (y2 - y1) / (x2 - x1) 
+*/
+
+struct point {
+	double 		x;
+	double 		y;
+};
+
+struct segment {
+	struct point 	a;
+	struct point 	b;
+};
+
+void
+print_segment(struct segment * s)
+{
+	printf("segment: A(%.2lf %.2lf) B(%.2lf %.2lf)\n", s->a.x, s->a.y, s->b.x, s->b.y);
+}
+
+struct segment *
+scan_new_segment()
+{
+	struct segment *s = NULL;
+	assert((s = malloc(sizeof(struct segment))) != NULL);
+
+	printf("input point one; eg: 23.2 -0.5 :\n");
+	scanf("%lf %lf", &(s->a.x), &(s->a.y));
+
+	printf("input point two; eg: 23.2 -0.5 :\n");
+	scanf("%lf %lf", &(s->b.x), &(s->b.y));
+
+	print_segment(s);
+	return s;
+}
+
 int
 main(void)
 {
-	double	s1p1, s1p2;
-	double	s2p1, s2p2;
+	printf("16_03\n");
+	struct segment *s;
 
-	printf("16_03...\n");
+	s = scan_new_segment();
 
-	/*
-	scanf("%d", &nconv);
-	scanf("%lf %c", &val, &unit);
-	*/
-	printf("input segment one ( eg: 23.2 -0.5 ):\n");
-	scanf("%lf %lf", &s1p1, &s1p2);
-	printf("segment one [ %lf, %lf ]\n", s1p1, s1p2);
-	printf("input segment two ( eg: 23.2 -0.5 ):\n");
-	scanf("%lf %lf", &s2p1, &s2p2);
-	printf("segment two [ %lf, %lf ]\n", s2p1, s2p2);
-
+	free(s);
 	return EXIT_SUCCESS;
 }
